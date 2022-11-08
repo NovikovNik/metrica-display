91edd9cd5e9d859109e30b2e0f822ef53f33d066
NovikovNik <nowickow.kol@yandex.ru>
2022-11-08 12:24:05 +0400

    typevar и mypy

--- metrics.py
+++ metrics.py
@@ -1,3 +1,4 @@
+from typing import TypeVar, Union, NoReturn, Callable, cast
 import psutil
 from rich.live import Live
 from rich.table import Table
@@ -17,27 +18,34 @@ class CpuIterator():
     def __next__(self):
         return self.get_curr_cpu(self.interval)
 
+from rich.console import ConsoleRenderable, RichCast
+ColumnName = TypeVar("ColumnName", bound=Union[ConsoleRenderable, RichCast, str])
 
 class TableFabric(Table):
     
-    def __init__(self, *args) -> None:
+    def __init__(self, *args: ColumnName) -> None:
         super().__init__()
         for i in args:
             self.add_column(header=i)
             
 
 def get_proc_percentage(cpu: float = 0) -> Table:
-    # table = Table()
-    table = TableFabric("Metric", "Description", "Value")
-    # table.add_column("Metric")
-    # table.add_column("Description")
-    # table.add_column("Value")
-    table.add_row('CPU', 'Current cpu usage', f"{str(cpu)}%")
-    return table
+    """ cast в except для mypy
+    """
+    try:
+        table = TableFabric("Metric", "Description", "Value")
+        table.add_row('CPU', 'Current cpu usage', f"{str(cpu)}%")
+        return table
+    except Exception:
+        cast(Callable[[], NoReturn], raise_exception)()
+
+
+def raise_exception():
+    raise Exception
 
 
 if __name__ == "__main__":
     with Live(get_proc_percentage(), refresh_per_second=1) as live:
         cpu = CpuIterator()
         for i in cpu:
-            live.update(get_proc_percentage(str(i)))
+            live.update(get_proc_percentage(i))
